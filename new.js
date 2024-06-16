26c68767b372afa10c8939649e33a1ae4c9cb27f
lfraundorfer <141073723+lfraundorfer@users.noreply.github.com>
2024-06-16 14:03:27 +0200

    Add a sticky navbar with anchor links

---
+++ new.js
@@ -0,0 +1,14 @@
+window.onscroll = function () {
+  myFunction();
+};
+
+var navbar = document.getElementById("menu");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.scrollY >= sticky) {
+    navbar.classList.add("sticky");
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
