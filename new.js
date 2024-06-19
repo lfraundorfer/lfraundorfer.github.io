6d138616811c10333277a7d76ecae446d1b98bc9
lfraundorfer <141073723+lfraundorfer@users.noreply.github.com>
2024-06-19 23:16:36 +0200

    Add a fade-in animation for the portfolio section on scroll

--- new.js
+++ new.js
@@ -1,14 +1,36 @@
-window.onscroll = function () {
-  myFunction();
-};
+document.addEventListener("DOMContentLoaded", function () {
+  window.onscroll = function () {
+    myFunction();
+  };
 
-var navbar = document.getElementById("menu");
-var sticky = navbar.offsetTop;
+  var navbar = document.getElementById("menu");
+  var sticky = navbar.offsetTop;
 
-function myFunction() {
-  if (window.scrollY >= sticky) {
-    navbar.classList.add("sticky");
-  } else {
-    navbar.classList.remove("sticky");
+  function myFunction() {
+    if (window.scrollY >= sticky) {
+      navbar.classList.add("sticky");
+    } else {
+      navbar.classList.remove("sticky");
+    }
   }
-}
+
+  const items = document.querySelectorAll(".appear");
+
+  const active = function (entries) {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("inview");
+      } else {
+        entry.target.classList.remove("inview");
+      }
+    });
+  };
+
+  const io2 = new IntersectionObserver(active, {
+    threshold: [0, 0.1, 0.5, 1], // Different thresholds for testing
+  });
+
+  items.forEach((item) => {
+    io2.observe(item);
+  });
+});
