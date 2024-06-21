96a2f7c1df4fb769b9477937156537019350ed85
lfraundorfer <141073723+lfraundorfer@users.noreply.github.com>
2024-06-22 00:30:58 +0200

    Add a quote section taken from a previous build

--- new.js
+++ new.js
@@ -35,4 +35,88 @@ document.addEventListener("DOMContentLoaded", function () {
     console.log("Observing row:", row); // Debugging log
     io2.observe(row);
   });
+
+  const testimonials = document.querySelector(".testimonials");
+  const nextButton = document.querySelector(".next-btn");
+  const prevButton = document.querySelector(".prev-btn");
+  const quotes = document.querySelectorAll(".quote");
+  const quoteIcons = document.querySelectorAll(".quote-slider-icon");
+  const numberOfSlides = quotes.length;
+  var slideNumber = 0;
+
+  //forward:
+  //make respective quote active
+  nextButton.addEventListener("click", () => {
+    slideNumber++;
+    quotes.forEach((quote) => {
+      quote.classList.remove("active");
+    });
+    quoteIcons.forEach((quoteIcon) => {
+      quoteIcon.classList.remove("active");
+    });
+
+    //go back to start at overflow
+    if (slideNumber > numberOfSlides - 1) {
+      slideNumber = 0;
+    }
+
+    quotes[slideNumber].classList.add("active");
+    quoteIcons[slideNumber].classList.add("active");
+    console.log(slideNumber);
+  });
+
+  //back:
+  //make respective quote active
+  prevButton.addEventListener("click", () => {
+    slideNumber--;
+    quotes.forEach((quote) => {
+      quote.classList.remove("active");
+    });
+    quoteIcons.forEach((quoteIcon) => {
+      quoteIcon.classList.remove("active");
+    });
+
+    //go back to start at overflow
+    if (slideNumber < 0) {
+      slideNumber = numberOfSlides - 1;
+    }
+
+    quotes[slideNumber].classList.add("active");
+    quoteIcons[slideNumber].classList.add("active");
+    console.log(slideNumber);
+  });
+
+  //automated play:
+  var playSlider;
+  var repeater = () => {
+    playSlider = setInterval(function () {
+      slideNumber++;
+      quotes.forEach((quote) => {
+        quote.classList.remove("active");
+      });
+      quoteIcons.forEach((quoteIcon) => {
+        quoteIcon.classList.remove("active");
+      });
+
+      //go back to start at overflow
+      if (slideNumber > numberOfSlides - 1) {
+        slideNumber = 0;
+      }
+
+      quotes[slideNumber].classList.add("active");
+      quoteIcons[slideNumber].classList.add("active");
+      console.log(slideNumber);
+    }, 4000);
+  };
+  repeater();
+
+  //stop automated play on mouseover
+  testimonials.addEventListener("mouseover", () => {
+    clearInterval(playSlider);
+  });
+
+  //start again on mouseout
+  testimonials.addEventListener("mouseout", () => {
+    repeater();
+  });
 });
