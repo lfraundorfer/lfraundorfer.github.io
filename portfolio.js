4c214c5a72bfb6781d97e9bc7892bc606d70ebab
lfraundorfer <141073723+lfraundorfer@users.noreply.github.com>
2024-07-15 23:14:00 +0200

    Load an array of images when clicking on a category

--- portfolio.js
+++ portfolio.js
@@ -2,18 +2,71 @@ document.addEventListener("DOMContentLoaded", function () {
   function getRandomInt(max) {
     return Math.floor(Math.random() * max);
   }
+
   const categories = document.querySelectorAll(".category");
 
-  categories.forEach((category) => {
+  const images_cat1 = [
+    'img/portfolio/cat1-1.jpg',
+  ];
+
+  const images_cat2 = [
+    'img/portfolio/cat2-1.jpg',
+    'img/portfolio/cat2-2.jpg',
+  ];
+
+  const images_cat3 = [
+    'img/portfolio/cat3-1.jpg',
+    'img/portfolio/cat3-2.jpg',
+    'img/portfolio/cat3-3.jpg',
+  ];
+
+  const images_cat4 = [
+    'img/portfolio/cat4-1.jpg',
+    'img/portfolio/cat4-2.jpg',
+    'img/portfolio/cat4-3.jpg',
+    'img/portfolio/cat4-4.jpg'
+  ];
+
+  function loadImages(images) {
+    const output = document.getElementById('output');
+    output.innerHTML = ''; // Clear existing images
+    images.forEach(src => {
+      const img = new Image();
+      img.src = src;
+      output.appendChild(img);
+    });
+  }
+
+  categories.forEach((category, index) => {
     category.addEventListener("click", function () {
       // Remove active class from all categories
       categories.forEach((cat) => cat.classList.remove("active"));
 
       // Add active class to the clicked category
       this.classList.add("active");
-      document.getElementById("reloadable").src =
-        "https://picsum.photos/200/" + getRandomInt(400);
-      console.log(getRandomInt(400));
+
+      // Load images based on the clicked category
+      switch (index) {
+        case 0:
+          loadImages(images_cat1);
+          break;
+        case 1:
+          loadImages(images_cat2);
+          break;
+        case 2:
+          loadImages(images_cat3);
+          break;
+        case 3:
+          loadImages(images_cat4);
+          break;
+      }
     });
   });
-});
+
+  categories[0].classList.add("active");
+  loadImages(images_cat1);
+
+  // document.querySelector('button').addEventListener('click', function () {
+  //   loadImages(images_cat1); // Initial load or button specific action
+  });
+
