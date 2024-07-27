9a517bca1eeeb11876c2150b2108781aa2db316e
lfraundorfer <141073723+lfraundorfer@users.noreply.github.com>
2024-07-27 10:22:44 +0200

    Adds the zoom animation, now replace the YT vid with it on index.html

--- zoom.js
+++ zoom.js
@@ -1,10 +1,10 @@
 const images = [
-    { src: './img/hero/crowd-heart.jpg', word: 'word1' },
-    { src: './img/hero/crowd-happy.jpg', word: 'word2' },
-    { src: './img/hero/fireworks.jpg', word: 'word3' },
-    { src: './img/hero/hercules.jpg', word: 'word4' },
-    { src: './img/hero/soccer.jpg', word: 'word5' },
-    { src: './img/hero/swimmer.jpg', word: 'word6' }
+    { src: './img/hero/webp/crowd-heart.webp', word: 'word1' },
+    { src: './img/hero/webp/crowd-happy.webp', word: 'word2' },
+    { src: './img/hero/webp/fireworks.webp', word: 'word3' },
+    { src: './img/hero/webp/hercules.webp', word: 'word4' },
+    { src: './img/hero/webp/soccer.webp', word: 'word5' },
+    { src: './img/hero/webp/swimmer.webp', word: 'word6' }
 ];
 
 const zoomDuration = 3000;
@@ -17,6 +17,13 @@ const imageElement = document.getElementById('animated-image');
 const textElement = document.getElementById('animated-text');
 const blackOverlay = document.getElementById('black-overlay');
 
+function applyZoomAnimation(element) {
+    element.style.animation = 'none'; // Reset animation
+    element.offsetHeight; // Trigger reflow
+    element.style.animation = ''; // Restart animation
+    element.style.animation = 'card-zoom 3s ease-in-out forwards';
+}
+
 function updateContent() {
     const currentImage = images[currentIndex];
     typeWriter(currentImage.word, () => {
@@ -25,6 +32,7 @@ function updateContent() {
                 currentIndex = (currentIndex + 1) % images.length; // Move this line here
                 const nextImage = images[currentIndex]; // Get the next image
                 imageElement.src = nextImage.src; // Set the next image
+                applyZoomAnimation(imageElement); // Apply zoom animation for new image
                 blackOverlay.style.opacity = 0;
                 setTimeout(updateContent, delayAfterErase);
             });
@@ -65,12 +73,14 @@ function erase(word, callback) {
 
 // Initial setup
 imageElement.src = images[currentIndex].src;
+applyZoomAnimation(imageElement); // Apply zoom animation for the first image
 typeWriter(images[currentIndex].word, () => {
     setTimeout(() => {
         erase(images[currentIndex].word, () => {
             currentIndex = (currentIndex + 1) % images.length;
             const nextImage = images[currentIndex];
             imageElement.src = nextImage.src;
+            applyZoomAnimation(imageElement); // Apply zoom animation for the first image
             blackOverlay.style.opacity = 0;
             setTimeout(updateContent, delayAfterErase);
         });
